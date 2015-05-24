077fae3561c5e693e2ec5faa2fc34b48fb16667f
nextdoorcompany <justin@nextdoorco.com>
2015-05-24 12:03:24 -0400

    added command line arg parsing

--- dir-backup.py
+++ dir-backup.py
@@ -5,15 +5,22 @@ import sys
 import os
 import datetime
 import shutil
+import argparse
 
-if len(sys.argv) < 3:
-    print('Usage: python dir-backup.py [source] [destination]')
-    sys.exit()
+parser = argparse.ArgumentParser()
+parser.add_argument('source', help = 'directory to copy')
+parser.add_argument('destination', help = 'directory to put copy')
+parser.add_argument('-m', '--move', help = 'delete source after copy', action = 'store_true')
+args = parser.parse_args()
 
-source = os.path.abspath(sys.argv[1])
-destination = os.path.join(sys.argv[2], datetime.date.today().strftime('%Y-%m-%d'))
+source = os.path.abspath(args.source)
+destination = os.path.join(args.destination, datetime.date.today().strftime('%Y-%m-%d'))
 
 print('source: %s\ndestination: %s' % (source, destination))
+if args.move:
+    print('move')
+
+sys.exit()
 
 if not os.path.exists(source) or not os.path.isdir(source):
     print('source invalid')
