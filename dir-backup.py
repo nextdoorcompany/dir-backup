18bb4a342045cea1b784941de4fd3baa12f14170
nextdoorcompany <justin@nextdoorco.com>
2015-05-24 12:30:24 -0400

    added move function

--- dir-backup.py
+++ dir-backup.py
@@ -16,11 +16,13 @@ args = parser.parse_args()
 source = os.path.abspath(args.source)
 destination = os.path.join(args.destination, datetime.date.today().strftime('%Y-%m-%d'))
 
-print('source: %s\ndestination: %s' % (source, destination))
+command_text = 'source: %s\ndestination: %s' % (source, destination)
 if args.move:
-    print('move')
+    command_text = 'move ->\n' + command_text
+else:
+    command_text = 'copy ->\n' + command_text
 
-sys.exit()
+print(command_text)
 
 if not os.path.exists(source) or not os.path.isdir(source):
     print('source invalid')
@@ -31,3 +33,6 @@ if os.path.exists(destination):
     sys.exit()
 
 shutil.copytree(source, destination)
+if args.move:
+    shutil.rmtree(source)
+    os.mkdir(source)
